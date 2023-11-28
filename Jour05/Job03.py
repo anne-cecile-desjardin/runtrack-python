1cdfe2681faf00110b776406ecb3370adcfb7fd7
Desjardin Anne-Cécile <134268553+anne-cecile-desjardin@users.noreply.github.com>
2023-11-28 10:08:44 +0100

    add job0102 03 04 05

--- Jour05/Job03.py
+++ Jour05/Job03.py
@@ -3,6 +3,19 @@
 #‘\’ et des ‘/’ en fonction de l’input entré, qui représentera la hauteur.
 #Exemple si l’input entré est 5 :
 
-#
-#
-#
+def draw_triangle(height):                                      #Function
+    for i in range(height):
+                                                                 # Show spaces before the caracteres
+        print(' ' * (height - i - 1), end='')
+
+        # Show the caractere '_' for the first line, '\', '/' for the other lines
+        if i == 0:
+            print('_')
+        elif i == height - 1:
+            print('/' + '_' * (2 * i - 1) + '\\')
+        else:
+            print('/' + ' ' * (2 * i - 1) + '\\')
+
+# How can we use draw_triangle(5) (example)
+draw_triangle(5)
+
--- Jour05/Job04.py
+++ Jour05/Job04.py
@@ -1,4 +1,16 @@
 #Écrire une fonction qui, recevant une taille n en paramètre, affiche un tapis de n+1
 #ligne/n+1 colonne traversé par une diagonale.
 
-#
+def afficher_tapis_diagonale(n):
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if i == j:
+                print('\\', end='')
+            elif i + j == n:
+                print('/', end='')
+            else:
+                print('#', end=' ')
+        print()
+
+# How to use the 4 size (example)
+afficher_tapis_diagonale(4)
